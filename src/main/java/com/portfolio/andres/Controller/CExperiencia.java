00c80bd6cba56b81774a235f3b625550487cda43
Andresvilla27 <108768637+Andresvilla27@users.noreply.github.com>
2023-03-01 00:34:27 -0300

    agregado de crossorigin a localhost

--- src/main/java/com/portfolio/andres/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/andres/Controller/CExperiencia.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "https://andres-villafane.web.app")
+@CrossOrigin(origins = {"https://andres-villafane.web.app","http://localhost:4200"})
 public class CExperiencia {
     @Autowired SExperiencia sExperiencia;
     
--- src/main/java/com/portfolio/andres/Controller/Chys.java
+++ src/main/java/com/portfolio/andres/Controller/Chys.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://andres-villafane.web.app")
+@CrossOrigin(origins = {"https://andres-villafane.web.app","http://localhost:4200"})
 @RequestMapping("/skill")
 public class Chys {
     @Autowired Shys shys;
