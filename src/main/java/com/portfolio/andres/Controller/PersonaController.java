71d9831bcb84a8f3901743dd40a269a1bc22ed9a
Andresvilla27 <108768637+Andresvilla27@users.noreply.github.com>
2023-03-03 00:08:59 -0300

    modificacion de crossorigin

--- src/main/java/com/portfolio/andres/Controller/PersonaController.java
+++ src/main/java/com/portfolio/andres/Controller/PersonaController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = {"https://andres-villafane.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://carlos-andres-villafane.web.app","http://localhost:4200"})
 public class PersonaController {
     @Autowired
     ImpPersonaService personaService;
