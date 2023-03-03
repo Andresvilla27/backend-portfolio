67cc053a366ce4ceaf731c2fef60bdd9bc9ff5b6
Andresvilla27 <108768637+Andresvilla27@users.noreply.github.com>
2023-03-03 01:19:54 -0300

    cambio en origin

--- src/main/java/com/portfolio/andres/Controller/CEducacion.java
+++ src/main/java/com/portfolio/andres/Controller/CEducacion.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = {"https://carlos-andres-villafane.web.app","http://localhost:4200"})
+@CrossOrigin(origins = "https://carlos-andres-villafane.web.app")
 public class CEducacion {
     @Autowired SEducacion sEducacion;
     
--- src/main/java/com/portfolio/andres/Controller/Chys.java
+++ src/main/java/com/portfolio/andres/Controller/Chys.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"https://carlos-andres-villafane.web.app","http://localhost:4200"})
+@CrossOrigin(origins = "https://carlos-andres-villafane.web.app")
 @RequestMapping("/skill")
 public class Chys {
     @Autowired Shys shys;
