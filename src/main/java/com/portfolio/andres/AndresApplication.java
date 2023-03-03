644acc0187c38d27d17e8944226a2fab55cc3d88
Andresvilla27 <108768637+Andresvilla27@users.noreply.github.com>
2023-03-03 11:53:02 -0300

    agregado cors en aplicacion principal

--- src/main/java/com/portfolio/andres/AndresApplication.java
+++ src/main/java/com/portfolio/andres/AndresApplication.java
@@ -2,6 +2,9 @@ package com.portfolio.andres;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AndresApplication {
@@ -9,5 +12,15 @@ public class AndresApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AndresApplication.class, args);
 	}
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("https://carlos-andres-villafane.web.app")
+                        .allowedMethods("*").allowedHeaders("*");
+		}
+            };
+        }
 
 }
